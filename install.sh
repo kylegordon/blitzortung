473741cf51594a98e129acba4479b2e2e4ca763b
Kyle Gordon <kyle@lodge.glasgownet.com>
2012-09-29 17:22:30 +0100

    Additions to build process

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 echo "src-link blitzortung /home/kyleg/src/blitzortung/" >> /home/kyleg/src/Bifferboard/bb/openwrt/feeds.conf.default
+cd /home/kyleg/src/Bifferboard/bb/openwrt/
+./scripts/feeds update blitzortung
 
